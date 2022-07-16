b4fcfda989ce99bcf36bcba7796064ed2c26e16a
Ahafonov-mykyta <ahafonovmykyta@gmail.com>
2022-07-16 21:56:23 +0300

    production

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import Notiflix from 'notiflix';
+import "simplelightbox/dist/simple-lightbox.min.css";
 import InfiniteScroll from  "infinite-scroll"
+import SimpleLightbox from "simplelightbox";
+
+
+
+
 
 let page =1;
 let searchName = "";
@@ -17,13 +23,15 @@ const API_KEY = '28643311-9e0ce007077315ffa397e3b9e';
 
 
 
-function fetchPhotos(name){
+async function fetchPhotos(name){
    
-    return fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`).then((response) => {
-       return response.json()})            
+     const response = await fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
+     const photos = response.json();
+     
+     return photos ; 
+               
 }
 
-// fetchPhotos("cat").then(data => createMarkup(data))
 
 
 function createMarkup (data) {   
@@ -35,10 +43,12 @@ function createMarkup (data) {
         return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
     }
 
-
+   
     data.hits.map(photo =>{
-    let markup = `<div class="photo-card">
-    <img src=${photo.webformatURL} alt=${photo.tags} loading="lazy" class="image"/>
+    let markup = `
+    <div class="photo-card"> 
+    <a href=${photo.largeImageURL}> 
+    <img src=${photo.webformatURL} alt=${photo.tags} loading="lazy" class="image"/> </a>
     <div class="info">
       <p class="info-item">
         <b>Likes</b></br>${photo.likes }
@@ -53,28 +63,31 @@ function createMarkup (data) {
         <b>Downloads</b>  </br> ${photo.downloads }
       </p>
     </div>
-  </div>`
+  </div>
+ `
     refs.gallery.insertAdjacentHTML("beforeEnd",markup)
+   
 
 })
 }
 
 
 
-
+var lightbox;
 const  onSubmit = (event) =>{
  event.preventDefault();
  page=1;
  refs.gallery.innerHTML="";
   searchName = refs.input.value;
-  refs.load.classList.remove("hidenn")
+  
  fetchPhotos(searchName)
  .then(data => { 
     if (data.hits.length === 0){
         refs.form.reset()
-        refs.load.classList.add(" hidenn")
+        refs.load.classList.add("hidenn")
         return Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-    } createMarkup(data); console.log(data)}).catch( error => console.log(error) );
+    }  createMarkup(data); console.log(data);Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images`)
+    refs.load.classList.remove("hidenn");lightbox = new SimpleLightbox('.gallery a', { enableKeyboard:	true});}).catch( error => console.log(error) );
 
  console.log(searchName);
 return searchName;
@@ -88,12 +101,22 @@ const loadMore = (event) => {
     page++;
     fetchPhotos(searchName)
     .then(data => {
-        if (data.hits.length === 0){
+        if (data.hits.length === 0 || page > 12){
         refs.load.classList.add("hidenn")
         return Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.") } ;
-        createMarkup(data)}).catch( error => console.log(error) );
+        createMarkup(data);scroll();lightbox.refresh()}).catch( error => console.log(error) );
 }
 
 refs.load.addEventListener("click", loadMore)
 
+function scroll(){
+const { height: cardHeight } = document
+  .querySelector(".gallery")
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: "smooth",
+});
+}
 
